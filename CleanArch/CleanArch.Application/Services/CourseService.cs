dc28159fe9c85150cbba79b0eefec0cce90b359a
Johan Potgieter <johan.ccs@gmail.com>
2019-08-11 11:08:09 +0200

    Commit Implementation of Service and Repositories  GetCourses Methods

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,34 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        #region Readonly Fields
+
+        private readonly ICourseRepository _courseRepository;
+
+        #endregion
+
+        #region Constructor
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        #endregion
+
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,31 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        #region Readonly Fields
+
+        private UniversityDBContext _ctx;
+
+        #endregion
+
+        #region Constructor
+
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        #endregion
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
